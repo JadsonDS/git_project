0a2a390d94c6775eef1fecdf1c96000fdc7ea06e
Jadson <jadson002@gmail.com>
2023-12-29 00:12:56 -0300

    Criado arquivo de extracao e ajustado arquivo principal

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
 
-def load_data():
-    return pd.read_csv('data/processed/bikes_completed.csv')
+import streamlit as st
+from src.extraction.py import load_data 
+
+st.set_page_config(layout="wide")
 
 def main():
     df=load_data()
