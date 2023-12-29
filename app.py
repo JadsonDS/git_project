7a121c0280ecb762bcd9663b593ec4051f602211
Jadson <jadson002@gmail.com>
2023-12-29 11:43:13 -0300

    Utilizado funcao  de descricao de dados

--- app.py
+++ app.py
@@ -39,9 +39,11 @@ def create_dataframe_section(df):
 
 
 def main():
-    df=load_data()
+    df_raw=load_data()
+    
+    create_dataframe_section(df_raw)
 
-    st.dataframe(df)
+    st.dataframe(df_raw)
     
 if __name__ == '__main__':
     main()    
